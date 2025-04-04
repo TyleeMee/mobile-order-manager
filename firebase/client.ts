7b131771d0cdd0ae6a1837a4a66f2a8fe5ad641a
TyleeMee <98672634+TyleeMee@users.noreply.github.com>
2025-04-04 12:30:42 +0900

    fix env file

---
+++ firebase/client.ts
@@ -0,0 +1,39 @@
+// Import the functions you need from the SDKs you need
+import { Analytics, getAnalytics } from "firebase/analytics";
+import { getApps, initializeApp } from "firebase/app";
+import { Auth, getAuth } from "firebase/auth";
+import { FirebaseStorage, getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const currentApps = getApps();
+let auth: Auth;
+let storage: FirebaseStorage;
+let analytics: Analytics;
+
+if (!currentApps.length) {
+  const app = initializeApp(firebaseConfig);
+  auth = getAuth(app);
+  storage = getStorage(app);
+  analytics = getAnalytics(app);
+} else {
+  const app = currentApps[0];
+  auth = getAuth(app);
+  storage = getStorage(app);
+  analytics = getAnalytics(app);
+}
+
+export { analytics, auth, storage };
---
+++ firebase/server.ts
@@ -0,0 +1,46 @@
+import {
+  cert,
+  getApps,
+  initializeApp,
+  ServiceAccount,
+} from "firebase-admin/app";
+import { Auth, getAuth } from "firebase-admin/auth";
+import { Firestore, getFirestore } from "firebase-admin/firestore";
+import { getStorage, Storage } from "firebase-admin/storage";
+
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+  universe_domain: "googleapis.com",
+};
+
+let firestore: Firestore;
+let storage: Storage;
+//!delete auth if you don't use context/action.ts file
+let auth: Auth;
+const currentApps = getApps();
+
+if (!currentApps.length) {
+  const app = initializeApp({
+    credential: cert(serviceAccount as ServiceAccount),
+    storageBucket: "mobile-order-manager-68e65.firebasestorage.app",
+  });
+  firestore = getFirestore(app);
+  storage = getStorage(app);
+  auth = getAuth(app);
+} else {
+  const app = currentApps[0];
+  firestore = getFirestore(app);
+  storage = getStorage(app);
+  auth = getAuth(app);
+}
+
+export { auth, firestore, storage };
